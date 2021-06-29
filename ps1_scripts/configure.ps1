f214db0eb4ae7465c94c6823a0837c0af46c7b39
Shingo Ohtsuka <capriciouswalk@gmail.com>
2021-06-29 19:48:34 +0900

    Update configure.ps1

--- ps1_scripts/configure.ps1
+++ ps1_scripts/configure.ps1
@@ -2,7 +2,9 @@ Param(
     [String] $xfm_native_root,
     [String] $pybind11_dir,
     [String] $config_file = '.config.json',
-    [String] $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    [String] $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe",
+    [String] $project_root_dir,
+    [String] $build_root_dir = 'build'
 )
 
 $config = ConvertFrom-Json '{}' -AsHashtable
@@ -10,18 +12,50 @@ if (Test-Path $config_file) {
     $config = Get-Content -Path $config_file | ConvertFrom-Json -AsHashtable
 }
 if ($xfm_native_root -ne '') {
-    $config['xfm_native_root'] = $xfm_native_root
+    $config['xfm_native_root'] = Convert-Path $xfm_native_root
 }
 if ($pybind11_dir -ne '') {
-    $config['pybind11_dir'] = $pybind11_dir
+    $config['pybind11_dir'] = Convert-Path $pybind11_dir
 }
+if ($project_root_dir -eq '') {
+    $project_root_dir = $MyInvocation.MyCommand.Path
+    foreach ($i in [System.Linq.Enumerable]::Range(0, 2)) {
+        $project_root_dir = Split-Path $project_root_dir -Parent
+    }
+}
+$project_root_dir = Convert-Path $project_root_dir
 
 $visual_studio = & "$vswhere" -prerelease -latest -products * -requires Microsoft.VisualStudio.Component.VC.CMake.Project -format json
 | ConvertFrom-Json
 
-# $test = pip show pybind11 | ForEach-Object{ "$_" -replace ": ","="} | ForEach-Object{ [Regex]::Escape(($_))} | ConvertFrom-StringData
-# Write-Host $test.Location
-# $pybind11_dir =  Join-Path $test.Location 'pybind11\share\cmake\pybind11' -Resolve
+# I'd like to unify build dirs between this and Visual Studio Code
+# https://github.com/microsoft/vscode-cmake-tools/blob/e86a553ec2712642ce5e44547e8e3d172537f029/src/kit.ts#L492
+function GetDisplayNameVS {
+    param (
+        [Parameter(Mandatory = $true)] [ValidateNotNullOrEmpty()] [PSCustomObject] $inst
+    )
+    if ($inst.displayName) {
+        if ($inst.channelId) {
+            $index = $inst.channelId.LastIndexOf('.')
+            if ($index -gt 0) {
+                return "$($inst.displayName) $($inst.channelId.Substring($index + 1))"
+            }
+        }
+        return $inst.displayName;
+    }
+    return $inst.instanceId;
+}
+
+function CMakePath {
+    param (
+        [Parameter(Mandatory = $true)] [ValidateNotNullOrEmpty()] [PSCustomObject] $visual_studio
+    )
+    $cmake = Join-Path `
+                $visual_studio.installationPath `
+                'Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe'`
+                -Resolve
+    return $cmake
+}
 
 function Configure {
     param (
@@ -33,10 +67,9 @@ function Configure {
         [Parameter(Mandatory = $true)] [ValidateNotNullOrEmpty()] [String] $build_target
     )
 
-    $cmake = Join-Path $visual_studio.installationPath 'Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe' -Resolve
+    $cmake = CMakePath($visual_studio)
 
-    $tool_name = $visual_studio.displayName
-    # https://github.com/microsoft/vscode-cmake-tools/blob/e86a553ec2712642ce5e44547e8e3d172537f029/src/kit.ts#L492
+    $tool_name = GetDisplayNameVS($visual_studio)
     $buildKit = "$tool_name - $build_target"
     $build_dir = Join-Path $build_root_dir "${buildKit}\Release"
     New-Item -ItemType Directory $build_dir -Force
@@ -71,15 +104,21 @@ function Configure {
         @vs_target                             `
         | Write-Host
 
-    return "$build_dir"
+    return Convert-Path $build_dir
 }
 
 Set-Variable -Name BUILD_TARGETS -Value @('x86', 'amd64') -Option Constant
 $build_dirs = @()
 
 foreach ($target in $BUILD_TARGETS) {
-    $dir = Configure -project_root_dir "." -build_root_dir "build"                                     `
+    $dir = Configure -project_root_dir $project_root_dir -build_root_dir $build_root_dir `
                      -xfm_native_root $config['xfm_native_root'] -pybind11_dir $config['pybind11_dir'] `
                      -visual_studio $visual_studio -build_target $target
-    $build_dirs += $dir
+    $build_dirs += $dir.FullName
 }
+
+$config['project_root_dir'] = $project_root_dir
+$config['build_dirs'] = $build_dirs
+$config['cmake'] = CMakePath($visual_studio)
+
+ConvertTo-Json $config | Out-File $config_file
